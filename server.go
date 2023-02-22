a207126e0ae50d0c5cbcc092df1a76d95098949c
Heitor Polizeli Rodrigues <heitorprodrigues@gmail.com>
2023-02-22 08:17:54 -0300

    build: :building_construction: create small go server with k8s configuration deployment in Kind

---
+++ server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/secret", Secret)
+	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8000", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s years old", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+
+	fmt.Fprintf(w, "My Famiily: %s.", string(data))
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "User: %s. Password %s", user, password)
+}
